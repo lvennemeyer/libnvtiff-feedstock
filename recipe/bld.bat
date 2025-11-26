5a67f94a98210f601f79bcf1501ccf6bfb6f2267
Lizzy Vennemeyer <lvennemeyer@nvidia.com>
2025-11-26 10:10:19 -0800

    Update for 0.6.0, single prefix layout

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,12 @@
+:: Relocate CUDA major specific libraries to single prefix layout
+
+for /f "tokens=1 delims=." %%a in ("%cuda_compiler_version%") do (
+    set "CUDA_MAJOR=%%a"
+    break
+)
+
+move lib lib.backup
+move lib.backup\%CUDA_MAJOR% lib
 
 if not exist %PREFIX% mkdir %PREFIX%
 
